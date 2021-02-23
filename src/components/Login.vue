5690efbe294be9db9b67e8852016dd84b1b360d4
YXH-code <2948511562@qq.com>
2021-02-23 17:06:15 +0800

    improve login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,21 @@
 <template>
-  <el-card>
-    user_name:<input type="text" v-model="loginForm.username"/>
-    <br><br>
-    password:<input type="password" v-model="loginForm.password"/>
-    <br><br>
-    <button @click="findUser">登录</button>
-  </el-card>
+  <body id="poster">
+    <el-form class="login-container" label-position="left"
+             label-width="0px">
+      <h3 class="login_title">登录</h3>
+      <el-form-item>
+        <el-input type="text" v-model="loginForm.username"
+                  auto-complete="off" placeholder="账号"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="password" v-model="loginForm.password"
+                  auto-complete="off" placeholder="密码"></el-input>
+      </el-form-item>
+      <el-form-item style="width: 100%">
+        <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </body>
 </template>
 
 <script>
@@ -14,14 +24,14 @@ export default {
   data () {
     return {
       loginForm: {
-        username: '',
-        password: ''
+        username: 'yxh',
+        password: '123'
       },
       responseResult: []
     }
   },
   methods: {
-    findUser: function () {
+    login: function () {
       this.$axios
         .post('/login', {
           username: this.loginForm.username,
@@ -41,5 +51,32 @@ export default {
 </script>
 
 <style scoped>
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 90px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
 
+  .login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+
+  #poster {
+    background:url("../assets/login.jpg") no-repeat;
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  body{
+    margin: 0px;
+  }
 </style>
